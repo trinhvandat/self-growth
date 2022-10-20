b7bc4f97d090e79bb3eae725c4f3da5c5d9d45c7
Nguyễn Song Toàn <nguyensongtoan0302@gmail.com>
2022-10-20 10:21:11 +0700

    core-email: implement method send mail with text

--- core/core-email/src/main/java/org/ptit/okrs/core_email/service/EmailServiceImpl.java
+++ core/core-email/src/main/java/org/ptit/okrs/core_email/service/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package org.ptit.okrs.core_email.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.thymeleaf.TemplateEngine;
@@ -23,7 +24,13 @@ public class EmailServiceImpl implements EmailService {
   @Async
   @Override
   public void send(String subject, String to, String content) {
-
+    log.info("(send)subject: {}, to: {}, content: {}", subject, to, content);
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(emailFrom);
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(content);
+    emailSender.send(message);
   }
 
   @Async
