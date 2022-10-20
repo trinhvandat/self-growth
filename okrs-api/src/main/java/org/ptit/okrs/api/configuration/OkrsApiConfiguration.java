b7a42dc2d67048b6c7400d7acd07fb6c8dac689d
trinh van dat <trinhvandat90399@gmail.com>
2022-10-20 18:29:37 +0700

    OKR-DDD Create core swagger

--- okrs-api/src/main/java/org/ptit/okrs/api/configuration/OkrsApiConfiguration.java
+++ okrs-api/src/main/java/org/ptit/okrs/api/configuration/OkrsApiConfiguration.java
@@ -2,10 +2,12 @@ package org.ptit.okrs.api.configuration;
 
 import org.ptit.okrs.core.configuration.EnableOkrsCore;
 import org.ptit.okrs.core_api_exception.configuration.EnableCoreApiException;
+import org.ptit.okrs.core_swagger.EnableCoreSwagger;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCoreApiException
+@EnableCoreSwagger
 @EnableOkrsCore
 public class OkrsApiConfiguration {
 }
