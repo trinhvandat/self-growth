cdb25810401048fe3942cdb5de1eb22704d5adc7
trinh van dat <trinhvandat90399@gmail.com>
2022-10-20 18:00:07 +0700

    OKR-DDD Update send mail with text

--- core/core-email/src/main/java/org/ptit/okrs/core_email/service/EmailServiceImpl.java
+++ core/core-email/src/main/java/org/ptit/okrs/core_email/service/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package org.ptit.okrs.core_email.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.ptit.okrs.core_exception.InternalServerError;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -25,12 +26,17 @@ public class EmailServiceImpl implements EmailService {
   @Override
   public void send(String subject, String to, String content) {
     log.info("(send)subject: {}, to: {}, content: {}", subject, to, content);
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setFrom(emailFrom);
-    message.setTo(to);
-    message.setSubject(subject);
-    message.setText(content);
-    emailSender.send(message);
+    try {
+      var message = new SimpleMailMessage();
+      message.setFrom(emailFrom);
+      message.setTo(to);
+      message.setSubject(subject);
+      message.setText(content);
+      emailSender.send(message);
+    } catch (Exception ex) {
+      log.error("(send)to: {}, subject: {}, ex: {}", to, subject, ex.getMessage());
+      throw new InternalServerError("Send mail failed to email: {}" + to);
+    }
   }
 
   @Async
