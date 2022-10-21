15d6193d6529827c00bd22e07ef9e2bb57362179
do duc anh <anh15042002hla@gmail.com>
2022-10-21 13:44:43 +0700

    fix: using constant, add @RequireArgConstructor and add @validate,

--- okrs-api/src/main/java/org/ptit/okrs/api/controller/NotificationController.java
+++ okrs-api/src/main/java/org/ptit/okrs/api/controller/NotificationController.java
@@ -1,42 +1,46 @@
 package org.ptit.okrs.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.ptit.okrs.api.model.request.NotificationRequest;
+import org.ptit.okrs.api.model.response.NotificationResponse;
 import org.ptit.okrs.core.entity.Notification;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.ptit.okrs.core.service.NotificationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.ptit.okrs.api.constant.OkrsApiConstant.BaseUrl.NOTIFICATION_BASE_URL;
+
 @RestController
-@RequestMapping("/api/v1/notification")
+@RequestMapping(NOTIFICATION_BASE_URL)
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationController {
 
-    private final NotificationService notificationService;
-
-    @Autowired
-    public NotificationController(NotificationService notificationService){
-        this.notificationService = notificationService;
-    }
+    private final NotificationService service;
 
     @PostMapping
-    public ResponseEntity<NotificationResponseDTO> createNotification(@RequestBody NotificationRequestDTO notificationRequestDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(notificationService.createNotification(notificationRequestDTO));
+    public ResponseEntity<NotificationResponse> createNotification(@Validated @RequestBody NotificationRequest notificationRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createNotification(notificationRequestDTO));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<NotificationResponseDTO> updateNotification(@PathVariable("id") int notificationId,
-                                                   @RequestBody NotificationRequestDTO notificationRequestDTO){
-        return ResponseEntity.status(HttpStatus.OK).body(notificationService.updateNotification(notificationId, notificationRequestDTO));
+    public ResponseEntity<NotificationResponse> updateNotification(@Validated @PathVariable("id") int notificationId,
+                                                                   @RequestBody NotificationRequest notificationRequestDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.updateNotification(notificationId, notificationRequestDTO));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<NotificationResponseDTO> deleteNotification(@PathVariable("id") int notificationId){
+    public ResponseEntity<NotificationResponse> deleteNotification(@PathVariable("id") int notificationId) {
         return ResponseEntity.status(HttpStatus.OK).body(notificationService.deleteNotification(notificationId));
     }
 
     @GetMapping
-    public ResponseEntity<List<Notification>> listNotifications(){
+    public ResponseEntity<List<Notification>> listNotifications() {
         return ResponseEntity.status(HttpStatus.OK).body(notificationService.listNotification());
     }
 
