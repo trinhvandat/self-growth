a82e7fc1fca2e9cfaf989514c2c091c3729de094
do duc anh <anh15042002hla@gmail.com>
2022-10-22 00:48:14 +0700

    fix constant url

--- okrs-api/src/main/java/org/ptit/okrs/api/constant/OkrsApiConstant.java
+++ okrs-api/src/main/java/org/ptit/okrs/api/constant/OkrsApiConstant.java
@@ -2,9 +2,7 @@ package org.ptit.okrs.api.constant;
 
 import static org.ptit.okrs.api.constant.OkrsApiConstant.ApiConstant.API_PREFIX;
 import static org.ptit.okrs.api.constant.OkrsApiConstant.ApiConstant.API_VERSION;
-import static org.ptit.okrs.api.constant.OkrsApiConstant.ResourceConstant.DAILY_PLAN;
-import static org.ptit.okrs.api.constant.OkrsApiConstant.ResourceConstant.KEY_RESULT;
-import static org.ptit.okrs.api.constant.OkrsApiConstant.ResourceConstant.OBJECTIVE;
+import static org.ptit.okrs.api.constant.OkrsApiConstant.ResourceConstant.*;
 
 public class OkrsApiConstant {
   public static class ApiConstant {
@@ -16,11 +14,14 @@ public class OkrsApiConstant {
     public static final String OBJECTIVE = "/objectives";
     public static final String DAILY_PLAN = "/daily-plans";
     public static final String KEY_RESULT = "/key-results";
+    public static final String NOTIFICATION = "/notifications";
+
   }
 
   public static class BaseUrl {
     public static final String OBJECTIVE_BASE_URL = API_PREFIX + API_VERSION + OBJECTIVE;
     public static final String DAILY_PLAN_BASE_URL = API_PREFIX + API_VERSION + DAILY_PLAN;
     public static final String KEY_RESULT_BASE_URL = API_PREFIX + API_VERSION + KEY_RESULT;
+    public static final String NOTIFICATION_BASE_URL = API_PREFIX + API_VERSION + NOTIFICATION;
   }
 }
