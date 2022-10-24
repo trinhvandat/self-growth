28a5c40ead02784448204e659c20c23b3fcc296f
trinh van dat <trinhvandat90399@gmail.com>
2022-10-24 21:13:42 +0700

    OKR-DDD add validator

---
+++ okrs-core/src/main/java/org/ptit/okrs/core/validation/ValidDateInteger.java
@@ -0,0 +1,32 @@
+package org.ptit.okrs.core.validation;
+
+import org.ptit.okrs.core_util.ValidationUtils;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = ValidDateInteger.DateIntegerValidator.class)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE, ElementType.FIELD})
+public @interface ValidDateInteger {
+
+  String message() default "Invalid date";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payloads() default {};
+
+  class DateIntegerValidator implements ConstraintValidator<ValidDateInteger, Integer> {
+
+    @Override
+    public void initialize(ValidDateInteger constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Integer date, ConstraintValidatorContext constraintValidatorContext) {
+      return ValidationUtils.validateDate(date);
+    }
+  }
+}
